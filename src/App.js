063fef126645f4d66324f5b04a062162d3a31ca9
Dennis Nemirovski <dennisnemirovski@gmail.com>
2021-11-13 22:46:35 +0200

    start landpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const  App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className='container'>
+    <img className='logo' src={logo}/>
+   <a className="link" href="https://spoti.fi/3cajJIY" >Spotify</a>
+   <a className="link" href="https://apple.co/3nbcuGM" >Apple Music</a>
+   <a className="link" href="https://apple.co/3nbcuGM" >Youtube</a>
+   <a className="link" href="https://apple.co/3nbcuGM" >Instagram</a>
+    </div>
     </div>
   );
 }
