89bcc245cab4e203d1af5d18acc818490c91a2a8
dennis1232 <dennisn@webiya.co.il>
2021-11-14 23:58:36 +0200

    change template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import instagramLogo from "./images/ig-instagram.svg";
+import appleLogo from "./images/apple-music.png";
+import spotifyLogo from "./images/spotify.svg";
+import album from "./images/album.png";
+import "./App.css";
 
-const  App=()=> {
+const App = () => {
   return (
     <div className="app">
-    <div className='container'>
-    <img className='logo' src={logo}/>
-   <a className="link" href="https://spoti.fi/3cajJIY" >Spotify</a>
-   <a className="link" href="https://apple.co/3nbcuGM" >Apple Music</a>
-   <a className="link" href="https://apple.co/3nbcuGM" >Youtube</a>
-   <a className="link" href="https://apple.co/3nbcuGM" >Instagram</a>
-    </div>
+      <div class="sound-icon disabled">
+        <div class="sound-wave">
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+          <i class="bar"></i>
+        </div>
+      </div>
+      <img className="album" src={album} />
+      <div className="container">
+        <a className="link" href="https://www.instagram.com/tomerbeneliezer/">
+          <img className="logo1" src={instagramLogo} />
+        </a>
+        <a className="link" href="https://apple.co/3nbcuGM">
+          <img className="logo2" src={appleLogo} />
+        </a>
+        <a className="link" href="https://apple.co/3nbcuGM">
+          <img className="logo3" src={spotifyLogo} />
+        </a>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
